bc1825e6447fc4f8137df66ad410fc16504a93c1
dev-z <ishtiaque.zafar92@gmail.com>
2022-06-04 19:32:14 +0200

    added create-list use case

---
+++ src/domain/todo/create-list.ts
@@ -0,0 +1,37 @@
+import createLogger from "@src/utils/logger";
+import { ListsRepository, List } from "@src/domain/types/todo";
+
+export interface Dependencies {
+  listsRepo: ListsRepository;
+}
+
+export type Input = {
+  list: Pick<List, "name">;
+  metadata: {
+    requestId: string;
+    actor: string;
+  };
+};
+
+export type CreateList = (input: Input) => Promise<List>;
+
+export default function createListAction(deps: Dependencies): CreateList {
+  const logger = createLogger("create-list");
+  return async (input: Input) => {
+    const { metadata } = input;
+    try {
+      const list = await deps.listsRepo.create(input.list.name);
+      logger.info("list created sucessfully", {
+        actor: metadata.actor,
+        name: input.list.name,
+      });
+      return list;
+    } catch (listErr) {
+      logger.error("failed to create list", {
+        actor: metadata.actor,
+        err: (<Error>listErr).message,
+        name: input.list.name,
+      });
+    }
+  };
+}
